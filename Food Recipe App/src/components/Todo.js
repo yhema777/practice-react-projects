de974c7d824e39f7c47706813a65a4e54c0958ab
yhema777 <yarraguntlahemasai@gmail.com>
2024-08-01 15:01:44 +0530

    added functionality

--- Food Recipe App/src/components/Todo.js
+++ Food Recipe App/src/components/Todo.js
@@ -1,49 +1,34 @@
-import { useState } from "react";
-import Todolist from "./Todolist";
-import '../css/Todo.css'
-
+import { useState, useSyncExternalStore } from "react";
+import "../css/Todo.css";
+import Card from "./Card";
+import mockdata from "../utilitis/mockdata";
 
 function Todo() {
-  const [note, setNote]=useState("");
-  const [todos,setTodos] = useState([]);
-  const [editIndex, setEditIndex] = useState(null);
-  
+  const [searchval,setSearchval] = useState("");
+  const [initialval, setInitialval] = useState(mockdata);
+  const [filteredval, setFilteredval] = useState(mockdata);
 
-  function Addbutton(){
-    if (note.trim()) {
-      const newTodos = editIndex !== null 
-        ? todos.map((todo, index) => (index === editIndex ? note : todo))
-        : [...todos, note];
-      setTodos(newTodos);
-      setNote("");
-      setEditIndex(null);
-    }
-    
-  }
-
-  function Deletebutton(indexValue){
-    const newTodos=todos.filter((todo,index)=> index !== indexValue);
-    setTodos(newTodos);
-  }
-
-  function Editbutton(indexValue){
-    setNote(todos[indexValue]);
-    setEditIndex(indexValue);
-    
+  function Onsearch(){
+    const Filteredoptions = initialval.filter(item=>item.title.toLowerCase().includes(searchval.toLowerCase()));
+    setFilteredval(Filteredoptions);
 
   }
 
-  return(
+
+  return (
     <div>
-      <h1>TODO LIST</h1>
-
-        <input value={note} type="text" onChange={e=>setNote(e.target.value)} placeholder="add item..."></input>
-        <button onClick={Addbutton}> {editIndex !== null ? "Update" : "ADD"}</button>
-        <Todolist todos={todos} deletefunction={Deletebutton} editfunction={Editbutton}/>
-        
+      <input type="text" onChange={(e)=>setSearchval(e.target.value)}></input>
+      <button onClick={Onsearch}>
+      Search
+      </button>
+      {filteredval.map((data) => (
+        <div >
+          <Card data={data} />
+       
+        </div>
+      ))}
     </div>
-  )
-  
+  );
 }
 
 export default Todo;
