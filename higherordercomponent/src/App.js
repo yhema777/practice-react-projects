5fa7943225043f38fb58f7f68504f656b6e0120c
Hema Yarraguntla <Hema.Yarraguntla@vistex.com>
2025-05-18 12:22:58 +0530

    Implemented HOC code

--- higherordercomponent/src/App.js
+++ higherordercomponent/src/App.js
@@ -4,37 +4,35 @@ import { useState } from 'react';
 function App() {
   return (
     <div className="flex flex-col items-center text-blue-400">
-      <Person1 />
-      <Person2 />
+      <Person1 pname="Ram"/>
+      <Person2 pname="Shyam"/>
     </div>
   );
 }
 
+function withMoneyLogic(Person, initialamount){
+  return function NewComponent(props) {
+    const [money, setMoney] = useState(initialamount);
+    function handleClick() {
+      setMoney(money * 2);
+    }
+    return <Person {...props} money={money} onClick={handleClick}/>
+  };
+}
 
-function Person1(){
-  const [money, setMoney] = useState(10);
-  function handleClick(){
-    setMoney(money*2);
-  }
+function Person({pname, money, onClick}){
   return (
     <div>
-      <p>Ram bets {money} rupees</p>
-      <button onClick={handleClick}>+</button>
+      <p>{pname} bets {money} rupees</p>
+      <button onClick={onClick}>+</button>
     </div>
+    
   )
 }
 
-function Person2(){
-  const [money, setMoney] = useState(50);
-  function handleClick(){
-    setMoney(money*2);
-  }
-  return (
-    <div>
-      <p>Shyam bets {money} rupees</p>
-      <button className="border-e-black" onClick={handleClick}>+</button>
-    </div>
-  )
-}
+
+const Person1 = withMoneyLogic(Person, 10);
+const Person2 = withMoneyLogic(Person, 50);
+
 
 export default App;
